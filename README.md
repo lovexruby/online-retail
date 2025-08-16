e891e3f3520cd01c25c917699ffa2a43b51d42f4
lovexruby <ruben.menc@hotmail.com>
2025-08-16 15:30:05 +0200

    RFM-Analysis

---
+++ README.md
@@ -0,0 +1,41 @@
+# Online Retail Analysis
+
+This project is a data analysis of the **UCI Online Retail Dataset**, providing insights into customer behavior, revenue trends, and product co-purchases.
+
+## Project Goals
+
+1. Identify the most valuable customers based on revenue.
+2. Analyze countries and time periods with the highest revenue.
+3. Explore products that are frequently purchased together (Market Basket Analysis).
+
+## Data Source
+
+- [UCI Online Retail Dataset](https://archive.ics.uci.edu/ml/datasets/Online+Retail)
+
+## Project Structure
+
+online-retail/
+│
+├── online_retail.csv # Original dataset or a sample
+├── analysis.ipynb # Python notebook with analysis
+├── analysis.py # Optional: Python script with analysis
+├── README.md # Project description
+└── .gitignore # Files/folders to ignore in Git
+
+
+## Analyses & Visualizations
+
+- Revenue per customer (Top Customers)
+- Revenue by country
+- Revenue trends over months
+- Frequently co-purchased products
+
+## How to Run
+
+1. Install Python 3.x
+2. Install required packages:
+```bash
+pip install pandas matplotlib
+Open the notebook or script and execute
+
+Visualizations will display charts and summaries
--- analysis.py
+++ analysis.py
@@ -43,7 +43,87 @@ df_positive = df[df['Quantity'] > 0]
 basket = df_positive.groupby('InvoiceNo')['StockCode'].apply(list)
 print(basket.head())
 
-revenue_month.plot(kind='line', figsize=(10,6), title='Revenue per month')
-plt.ylabel('Revenue')
-plt.xlabel('Month')
+
+# RFM-Analysis
+# Filter out customers with Nan CustomerID
+df_cust = df.dropna(subset=['CustomerID']).copy()
+df_cust['CustomerID'] = df_cust['CustomerID'].astype(int)
+
+# Filter out retours
+mask_returns = (
+    df_cust['InvoiceNo'].astype(str).str.startswith('C') |
+    (df_cust['Quantity'] <= 0) |
+    (df_cust['UnitPrice'] <= 0)
+)
+df_rfm = df_cust.loc[~mask_returns].copy()
+
+# Referencedate: 1 day after last purchase
+snapshot_date = df_rfm['InvoiceDate'].max() + pd.Timedelta(days=1)
+
+rfm = (
+    df_rfm
+        .groupby('CustomerID')
+        .agg(
+        Recency=('InvoiceDate',lambda x: (snapshot_date - x.max()).days),
+        Frequency=('InvoiceNo', 'nunique'),
+        Monetary=('Revenue', 'sum')
+    )
+    .reset_index()
+)
+# Safety prevention. Negative/Null Monetary shouldn`t exist
+rfm = rfm[rfm['Monetary'] > 0]
+
+# Convert into quintiles
+# Recency, smaller score better
+recency_ranks = rfm['Recency'].rank(method='first', ascending=False)
+rfm['R_Score'] = pd.qcut(recency_ranks, 5, labels=[1,2,3,4,5]).astype(int)
+# Frequency, higher score better
+recency_ranks = rfm['Frequency'].rank(method='first', ascending=True)
+rfm['F_Score'] = pd.qcut(recency_ranks, 5, labels=[1,2,3,4,5]).astype(int)
+# Monetary higher score better
+recency_ranks = rfm['Monetary'].rank(method='first', ascending=True)
+rfm['M_Score'] = pd.qcut(recency_ranks, 5, labels=[1,2,3,4,5]).astype(int)
+
+# Totalscore
+rfm['RFM_Score'] = (
+    rfm['R_Score'].astype(str) + rfm['F_Score'].astype(str) + rfm['M_Score'].astype(str)
+)
+rfm['RFM_Total'] = rfm[['R_Score', 'F_Score', 'M_Score']].sum(axis=1)
+
+# Define Segments
+def segment(row):
+    r, f, m = row['R_Score'], row['F_Score'], row['M_Score']
+    if r >= 4 and f >= 4:
+        return 'Champions'
+    if r >= 4 and f <= 2:
+        return 'New Customer'
+    if r <= 2 and f >= 4:
+        return 'At-Risk Loyal'
+    if r <= 2 and f <= 2:
+        return 'Lost'
+    if r >= 4 and m >= 4:
+        return 'Loyal Big Spenders'
+    return 'Potential Loyalist'
+
+rfm['Segment'] = rfm.apply(segment, axis=1)
+
+# Segment distribution
+print(rfm['Segment'].value_counts().sort_values(ascending=False))
+
+# Top Customer per Segment
+top_by_segment = (
+    rfm.sort_values('Monetary',ascending=False)
+        .groupby('Segment')
+        .head(5)
+        .reset_index(drop=True)
+)
+print(top_by_segment.head(20))
+
+
+rfm['Segment'].value_counts().plot(kind='bar', figsize=(10,8.5), title='Customers per Segment')
+plt.ylabel('Count')
+#plt.xlabel('Month')
+#plt.subplots_adjust(top=1.02)
 plt.show()
+
+rfm.to_csv('rfm_results.csv', index=False)
